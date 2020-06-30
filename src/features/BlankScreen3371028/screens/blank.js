6544359d921650e16b855417d7c6d0a61776c889
Crowdbotics <team@crowdbotics.com>
2020-06-30 21:52:26 +0000

    Update components

--- src/features/BlankScreen3371028/screens/blank.js
+++ src/features/BlankScreen3371028/screens/blank.js
@@ -376,7 +376,38 @@ export class _Blank extends React.Component {
                     borderRadius: 0,
                     backgroundSize: "auto"
                   }}
-                />
+                >
+                  <Text
+                    style={{
+                      marginLeft: 0,
+                      marginRight: 0,
+                      marginTop: 5,
+                      marginBottom: 5,
+                      paddingLeft: 5,
+                      paddingRight: 5,
+                      paddingTop: 5,
+                      paddingBottom: 5,
+                      overflow: "visible",
+                      textAlign: "left",
+                      verticalAlign: "baseline",
+                      fontSize: 20,
+                      color: "#fd6f80",
+                      fontStyle: "normal",
+                      fontWeight: "normal",
+                      fontFamily: "System",
+                      borderColor: "#000000",
+                      borderStyle: "solid",
+                      borderWidth: 0,
+                      borderLeftWidth: 0,
+                      borderRightWidth: 0,
+                      borderTopWidth: 0,
+                      borderBottomWidth: 0,
+                      borderRadius: 0
+                    }}
+                  >
+                    Sample text content
+                  </Text>
+                </View>
               </View>
             </View>
             <View
