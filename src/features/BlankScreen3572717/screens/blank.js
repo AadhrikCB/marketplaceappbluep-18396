fd63f878197d6fa1d1e434dd4ea6d86fae94481f
Crowdbotics <team@crowdbotics.com>
2020-07-07 17:45:05 +0000

    Update components

--- src/features/BlankScreen3572717/screens/blank.js
+++ src/features/BlankScreen3572717/screens/blank.js
@@ -1821,73 +1821,6 @@ export class _Blank extends React.Component {
                                       $25
                                     </Text>
                                   </View>
-                                  <View
-                                    style={{
-                                      width: "50%",
-                                      height: "100%",
-                                      marginLeft: 0,
-                                      marginRight: 0,
-                                      marginTop: 0,
-                                      marginBottom: 0,
-                                      paddingLeft: 0,
-                                      paddingRight: 0,
-                                      paddingTop: 0,
-                                      paddingBottom: 0,
-                                      overflow: "visible",
-                                      textAlign: "left",
-                                      verticalAlign: "baseline",
-                                      fontSize: 20,
-                                      color: "#000000",
-                                      backgroundColor: "#ffffff",
-                                      fontStyle: "normal",
-                                      fontWeight: "normal",
-                                      flexDirection: "column",
-                                      flexWrap: "wrap",
-                                      justifyContent: "flex-start",
-                                      alignItems: "stretch",
-                                      alignContent: "stretch",
-                                      borderColor: "#000000",
-                                      borderStyle: "solid",
-                                      borderWidth: 0,
-                                      borderLeftWidth: 0,
-                                      borderRightWidth: 0,
-                                      borderTopWidth: 0,
-                                      borderBottomWidth: 0,
-                                      borderRadius: 0,
-                                      backgroundSize: "auto"
-                                    }}
-                                  >
-                                    <Text
-                                      style={{
-                                        marginLeft: 0,
-                                        marginRight: 0,
-                                        marginTop: 5,
-                                        marginBottom: 5,
-                                        paddingLeft: 5,
-                                        paddingRight: 5,
-                                        paddingTop: 5,
-                                        paddingBottom: 5,
-                                        overflow: "visible",
-                                        textAlign: "right",
-                                        verticalAlign: "baseline",
-                                        fontSize: 10,
-                                        color: "#5D5B5D",
-                                        backgroundColor: "#ffffff",
-                                        fontStyle: "normal",
-                                        fontWeight: "normal",
-                                        borderColor: "#000000",
-                                        borderStyle: "solid",
-                                        borderWidth: 0,
-                                        borderLeftWidth: 0,
-                                        borderRightWidth: 0,
-                                        borderTopWidth: 0,
-                                        borderBottomWidth: 0,
-                                        borderRadius: 0
-                                      }}
-                                    >
-                                      Jan 5, 2020
-                                    </Text>
-                                  </View>
                                 </View>
                               </View>
                             </View>
@@ -2819,18 +2752,22 @@ export class _Blank extends React.Component {
                             }}
                           >
                             <Image
-                              resizeMode="cover"
+                              resizeMode="contain"
+                              source={{
+                                uri:
+                                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/home_1.png"
+                              }}
                               style={{
-                                width: "100%",
-                                height: "100%",
+                                width: "88%",
+                                height: "88%",
                                 marginLeft: 0,
                                 marginRight: 0,
                                 marginTop: 0,
                                 marginBottom: 0,
-                                paddingLeft: 0,
-                                paddingRight: 0,
-                                paddingTop: 0,
-                                paddingBottom: 0,
+                                paddingLeft: "6%",
+                                paddingRight: "6%",
+                                paddingTop: "6%",
+                                paddingBottom: "6%",
                                 overflow: "visible",
                                 textAlign: "left",
                                 verticalAlign: "baseline",
