5d4f345e5fb23f6330cf795d25eb0ed352e2781d
Crowdbotics <team@crowdbotics.com>
2020-07-09 20:26:06 +0000

    Update components

--- src/features/BlankScreen3271029/screens/blank.js
+++ src/features/BlankScreen3271029/screens/blank.js
@@ -1053,7 +1053,7 @@ export class _Blank extends React.Component {
               verticalAlign: "baseline",
               fontSize: 20,
               color: "#000000",
-              backgroundColor: "#ffffff",
+              backgroundColor: "#f4f5f9",
               fontStyle: "normal",
               fontWeight: "normal",
               flexDirection: "column",
@@ -1071,7 +1071,38 @@ export class _Blank extends React.Component {
               borderRadius: 0,
               backgroundSize: "auto"
             }}
-          />
+          >
+            <Image
+              resizeMode="contain"
+              source={{
+                uri:
+                  "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Screen_Shot_2020-07-09_at_3.24.41_PM.png"
+              }}
+              style={{
+                width: "100%",
+                height: "85%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 0,
+                marginBottom: 0,
+                paddingLeft: 0,
+                paddingRight: 0,
+                paddingTop: 0,
+                paddingBottom: 0,
+                overflow: "visible",
+                textAlign: "left",
+                verticalAlign: "baseline",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0
+              }}
+            />
+          </View>
           <View
             style={{
               width: "100%",
