e3f3b826040ffc461ddeef3d896af885e205f853
AadhrikCB <66269771+AadhrikCB@users.noreply.github.com>
2020-07-16 17:02:20 -0400

    changed background color 33

--- src/features/BlankScreen3371028/screens/blank.js
+++ src/features/BlankScreen3371028/screens/blank.js
@@ -38,7 +38,7 @@ export class _Blank extends React.Component {
         verticalAlign: "baseline",
         fontSize: 20,
         color: "#000000",
-        backgroundColor: "#f4f5f9",
+        backgroundColor: "#00ff00",
         fontStyle: "normal",
         fontWeight: "normal",
         fontFamily: "System",
